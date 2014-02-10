63f73d8f60d0dc77d392fa7f6817d6719aa72ae5
Jonathan MacDonald <jonathan.macdonald@gmail.com>
2014-02-10 16:27:25 -0600

    Update row.bat

--- row.bat
+++ row.bat
@@ -1,7 +1,6 @@
 
-
 set PATH=c:\windows\system32;c:\windows
 set PATH=%PATH%;c:\row\Ruby200\bin
-
+set PATH=%PATH%;C:\row\mysql-connector-c-noinstall-6.0.2-win32\lib
 
 start "Rails on Windows" /D c:\row
